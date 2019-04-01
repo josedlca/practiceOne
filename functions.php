972cb4f40eaeb70ade1b2a8743f62c82f60f6133
josedlca <jose.dlcruz.ayala@gmail.com>
2019-04-01 16:51:45 -0500

    navwalker, header

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	==========================================
+	 Theme support function
+	==========================================
+*/
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('post-thumbnails');
+/*
